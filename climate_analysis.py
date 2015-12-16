0a5cbfc8340f09b1c235ab7dad185e9c25f2084a
Aurel <aurelienbeaumais@hotmail.fr>
2015-12-16 10:38:16 +0000

    Modification 3

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,6 +1,6 @@
 climate_data = open('sc_climate_data10.csv', 'r')
 
-""" Climate Analysis Tools 3 """
+""" Climate Analysis Tools 6 """
 import temp_conversion
 import signal
 signal.signal(signal.SIGPIPE,
