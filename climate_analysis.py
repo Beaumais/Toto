18a05bea0f8c4f66ecd9757c6f6ee88e1d958a24
Aurel <aurelienbeaumais@hotmail.fr>
2015-12-16 10:42:04 +0000

    I let a message without -m

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,6 +1,6 @@
 climate_data = open('sc_climate_data10.csv', 'r')
 
-""" Climate Analysis Tools 7 """
+""" Climate Analysis Tools 8 """
 import temp_conversion
 import signal
 signal.signal(signal.SIGPIPE,
