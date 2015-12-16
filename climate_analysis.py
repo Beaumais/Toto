5829e940db06ad51ddb10b82740946ceaadcfe40
Aurel <aurelienbeaumais@hotmail.fr>
2015-12-16 10:20:44 +0000

    add docstring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,27 +1,24 @@
-import sys
+climate_data = open('sc_climate_data10.csv', 'r')
+
+""" Climate Analysis Tools """
 import temp_conversion
 import signal
-signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+signal.signal(signal.SIGPIPE,
+signal.SIG_DFL)
+import sys
 
-script = sys.argv[0]
-assert len(sys.argv) == 2, script + ": requires filename"
 filename = sys.argv[1]
-
 climate_data = open(filename, 'r')
-
 for line in climate_data:
-    data = line.split(',')
-
-    if data[0][0] == '#':
-        # don't want to process comment lines, which start with '#'
-        pass
-    else:
-        # extract our max temperature in Fahrenheit - 4th column
-        fahr = float(data[3])
-
-        # don't process invalid temperature readings of -9999
-        if fahr != -9999:
-            celsius = temp_conversion.fahr_to_celsius(fahr)
-            kelvin = temp_conversion.fahr_to_kelvin(fahr)
-
-            print(str(celsius)+", "+str(kelvin))
+	data = line.rstrip()
+	data = data.split(',')
+	if data[0][0] == '#':
+		# don't want to process comment lines, which start with '#'
+		pass
+	else:
+		# print 4th column (max temperature)
+		fahr = float(data[3])
+		celsius = temp_conversion.fahr_to_celsius(fahr)
+		kelvin = temp_conversion.fahr_to_kelvin(fahr)
+		print('Max temperature', data[3])
+		print('Max temperature in Celsius:', celsius, 'kelvin', kelvin)
